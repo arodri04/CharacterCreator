0fcd70cd05ed72726d6885653b4b9aa0eadc16bd
arodri04 <augustine.rodriguez55@gmail.com>
2024-12-03 15:20:22 -0700

    setting up flask

---
+++ app/views.py
@@ -0,0 +1,14 @@
+###THIS IS FOR OUR ROUTES###
+from app import app
+
+@app.route('/')
+def index():
+    return 'Homepage'
+
+@app.route('/patients')
+def list_of_patients():
+    return 'List of patients'
+
+@app.route('/patient')
+def individual_patient():
+    return 'Patient Information'
